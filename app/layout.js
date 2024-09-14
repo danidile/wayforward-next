fecd07e6637806a72af493a61a991fd7aa77ee7c
Daniele Di Lecce <alessialauriero@Dani-Mac.local>
2024-09-14 11:22:19 +0200

    added vercel/speed-insights

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import NavBar from "./components/navigation";
 import Footer from "./components/footer";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -23,6 +24,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+      <SpeedInsights/>
 
       <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..300,0..1,-50..200" />
       <link href='https://fonts.googleapis.com/css2?family=Montserrat' rel='stylesheet' type='text/css'/>
