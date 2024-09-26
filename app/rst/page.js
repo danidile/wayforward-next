e9b046e083a5d1c1fcf5de5f61acd71fa6547102
Daniele Di Lecce <alessialauriero@Dani-Mac.local>
2024-09-26 18:00:34 +0200

    third commit for resposiveness csss

--- app/rst/page.js
+++ app/rst/page.js
@@ -3,23 +3,25 @@ import styles from "./page.module.css";
 import Script from "next/script";
 
 
-export default function Home() {
+export default function rst() {
   return (<>
 
-    <div className="hero-section">
-            <div className="hero-gradient"></div>
+    <div className="rst-section">     
+                <div class="rst-container">
+                    <img src="/images/rst2.jpg"/>
+                    <div class="rst-content">
+                        <h3>Resident Support Team</h3>
+                        <h6>
+                            Join hundreds of people impacting their local communities. 
+                        </h6>
+                        <p>
+                            Resident Support Teams (RTSs) are Christians 18 years and older passionate about engaging their neighbors with the gospel and the 5 C’s while living in multi-housing communities nationwide. 
+                        </p>
+                    </div>
+                    
+                
+                </div>      
 
-            <img src="/images/rst.jpg" className="rst-hero-image"></img>
-           
-               <h1>Resident Support Team</h1>
-               <h6>
-                   Join hundreds of people impacting their local communities. 
-               </h6>
-               <p>
-                   Resident Support Teams (RTSs) are Christians 18 years and older passionate about engaging their neighbors with the gospel and the 5 C’s while living in multi-housing communities nationwide. 
-               </p>
-               
-           
        </div>
        <div className="fc-section">
                <div className="fc-content">
@@ -33,50 +35,72 @@ export default function Home() {
                    </ul>
                </div>
        </div>
-        <div className="steps-section">
-            <h2>Follow the steps.</h2>
-            <div className="steps-container">
-                <div className="steps-card"><h6 className="steps-title">APPLY</h6></div>
-                <div className="steps-card"><img src="/images/arrow.png" className="steps-arrow"/></div>
-                <div className="steps-card"><h6 className="steps-title">GET TRAINED</h6></div>
-                <div className="steps-card"><img src="/images/arrow.png" className="steps-arrow" /></div>
-                <div className="steps-card"><h6 className="steps-title">MOVE IN</h6></div>
-                <div className="steps-card"><img src="/images/arrow.png" className="steps-arrow" /></div>
-                <div className="steps-card"><h6 className="steps-title">IMPACT</h6></div>
-            </div>
-        </div>
-        <div className="apply-section">
-            <div className="apply-container">
-                <img className="apply-img" src="/images/church.jpg" alt=""/>
-                <div className="apply-content">
-                    <h1 className="apply-title">01.APPLY</h1>
-                    <h3>Apply today!</h3>
-                    <p>click the button below to apply and start your journey!</p>
-                    
-                    <a href="" className="button-big">
-                        <div>
-                            Let's partner
-                        </div>
-                    </a>              
-                    </div>
-            </div>
-        </div>
-        <div className="apply-section">
-            <div className="apply-container">
-                <div className="apply-content">
-                    <h1 className="apply-title">OPENINGS</h1>
-                    <h3>Current WayForward Openings</h3>
-                    <p>click the button below to apply and start your journey!</p>
-                    <a href="" className="button-big">
-                        <div>
-                            Let's partner
-                        </div>
-                    </a>
-                </div>
-                <img className="apply-img" src="/images/map.jpg" alt=""/>
+         
+        <div className="rst-section">
+        <h2>Follow the steps.</h2>
 
-            </div>
-        </div>
+                <div class="rst-container">
+                    <>
+                    <img src="/images/church.jpg"/>
+                    <div class="rst-content">
+                        <h3>01.APPLY</h3>
+                        <h6>
+                        Apply today!
+                        </h6>
+                        <p>
+                        click the button below to apply and start your journey!
+                        </p>
+                        
+                                <a className="button-big" href="/partner">
+                                    Let's partner  
+                                </a>
+                                                          
+                    </div>
+                    </>
+                    <>
+                    <div class="rst-content">
+                        <h3>02.Get Trained</h3>
+                        <h6>
+                        Apply today!
+                        </h6>
+                        <p>
+                        click the button below to apply and start your journey!
+                        </p>
+                    </div>
+                    <img src="/images/rst1.jpg"/>
+
+                    </>
+                    <>
+                    <img src="/images/map.jpg"/>
+
+                    <div class="rst-content">
+                        <h3>03.Move in</h3>
+                        <h6>
+                        Apply today!
+                        </h6>
+                        <p>
+                        click the button below to apply and start your journey!
+                        </p>
+                    </div>
+
+                    </>
+                    <>
+                    <div class="rst-content">
+                        <h3>04.Impact</h3>
+                        <h6>
+                        Apply today!
+                        </h6>
+                        <p>
+                        click the button below to apply and start your journey!
+                        </p>
+                    </div>
+                    <img src="/images/organizations.jpg"/>
+
+                    </>
+                
+                </div>      
+
+       </div>
 
 
 
